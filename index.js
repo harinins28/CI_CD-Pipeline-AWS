c90243523a0a5746d1ffcfe73baa17955920dc21
harinins28 <hariniinagarathinam@gmail.com>
2025-11-03 12:20:59 +0530

    index update check 6

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res)=> res.send('Hello from CI-CD app!, The app is running fine. Adding a line. Check if it works.'));
-app.get('/', (req,res)=> res.send('Second'));
+app.get('/', (req,res)=> res.send('Hello from CI-CD app!, The app is running fine. Adding a line. Check if it works.red me'));
 app.get('/health', (req,res)=> res.json({status:'ok'}));
 
 app.listen(port, ()=> console.log('Listening on', port));
