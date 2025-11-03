2e006ce17b79fe60aa86d4d298f399a8dfcdbffa
harinins28 <hariniinagarathinam@gmail.com>
2025-11-03 10:22:16 +0530

    index line

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res)=> res.send('Hello from CI-CD app!, The app is running fine.'));
+app.get('/', (req,res)=> res.send('Hello from CI-CD app!, The app is running fine. Adding a line'));
 app.get('/health', (req,res)=> res.json({status:'ok'}));
 
 app.listen(port, ()=> console.log('Listening on', port));
