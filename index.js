8cffcd759321af6b7ba57cc6b02484a37b009e19
harinins28 <hariniinagarathinam@gmail.com>
2025-11-04 10:40:06 +0530

    Commit check 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req,res)=> res.send('Hello from CI-CD app! Change one'));
+app.get('/', (req,res)=> res.send('Heello from CI-CD app! Change one'));
 app.get('/health', (req,res)=> res.json({status:'ok'}));
 
 app.listen(port, ()=> console.log('Listening on', port));
